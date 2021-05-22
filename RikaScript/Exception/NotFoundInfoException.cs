3d45295d69e6a864848d75e3b508358c17ecffc2
迈向黎明 <2293840045@qq.com>
2021-05-22 15:50:29 +0800

    v0.4.1 - 2021年5月22日，类库帮助标准化更新、扩展if @var return语法

---
+++ RikaScript/Exception/NotFoundInfoException.cs
@@ -0,0 +1,9 @@
+﻿namespace RikaScript.Exception
+{
+    public class NotFoundInfoException:RikaScriptException
+    {
+        public NotFoundInfoException(string info) : base("找不到相关信息：" + info)
+        {
+        }
+    }
+}
